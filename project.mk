4b385f8616826618fba80402e467999458d6f239
Tobias Kässer <tobias.kasser@upbound.io>
2024-11-12 10:14:43 +0100

    Update to uptest v1

---
+++ project.mk
@@ -0,0 +1,3 @@
+PROJECT_NAME := configuration-aws-database
+UPTEST_INPUT_MANIFESTS := ./examples/network.yaml,./examples/mariadb-claim.yaml,./examples/postgres-claim.yaml
+UPTEST_SKIP_UPDATE := true
